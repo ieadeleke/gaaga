cda3d6d70e77561d8ba15de7152eea192aea23ba
ieadeleke <eadelekeife@yahoo.com>
2026-03-23 13:09:30 +0100

    gaaga pages cleanup

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -2,6 +2,8 @@ import BookingHero from "@/features/booking/components/Hero";
 import BookingForm from "@/features/booking/components/BookingForm";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 const BookingPage = () => {
   return (
     <div className="bg-[#111]">
